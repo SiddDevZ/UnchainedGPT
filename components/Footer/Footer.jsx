951be89b333329478c226ee03ffc24acf9fcadd0
SiddDevZ <epic.siddarth@gmail.com>
2025-02-20 00:35:08 +0530

    logo changed

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -5,7 +5,11 @@ const Footer = () => {
     <div className="w-full flex flex-col xss:items-center sm:flex-row sm:justify-between mt-[10rem] border-t py-10">
       <div className="xss:mb-8 sm:mb-0 xss:text-center sm:text-left">
         <div className="flex xss:justify-center sm:justify-start items-center space-x-3 font-bold">
-          <i className="h-5 w-6 bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm"></i>
+          <img
+            src="/logo.webp"
+            alt="Zenos AI Logo"
+            className="sm:w-8 sm:h-8 xss:w-7 xss:h-7"
+          />
           <h2 className="text-[1.5rem] text-[#FFFFFF] leading-5 font-bold">
             Zenos AI
           </h2>
@@ -23,9 +27,21 @@ const Footer = () => {
         </p>
       </div>
       <div className="flex xss:justify-center sm:justify-start items-center space-x-3 text-sm">
-        <a href="mailto:siddz.dev@gmail.com" className="ri-at-line sm:text-[2.2rem] xss:text-[2rem] text-black dark:text-white transition-all duration-300 ease-in-out hover:scale-110 cursor-pointer"></a>
-        <a href="https://discord.com/users/273352781442842624" className="ri-discord-fill sm:text-[2.2rem] xss:text-[2rem] text-black dark:text-white transition-all duration-300 ease-in-out hover:scale-110 cursor-pointer"></a>
-        <a href="https://github.com/SiddDevZ" className="ri-github-fill sm:text-[2.2rem] xss:text-[2rem] text-black dark:text-white transition-all duration-300 ease-in-out hover:scale-110 cursor-pointer"></a>
+        <a
+          target="_blank"
+          href="mailto:siddz.dev@gmail.com"
+          className="ri-at-line sm:text-[2.2rem] xss:text-[2rem] text-black dark:text-white transition-all duration-300 ease-in-out hover:scale-110 cursor-pointer"
+        ></a>
+        <a
+          target="_blank"
+          href="https://x.com/askzenos"
+          className="ri-twitter-x-fill sm:text-[2.2rem] xss:text-[2rem] text-black dark:text-white transition-all duration-300 ease-in-out hover:scale-110 cursor-pointer"
+        ></a>
+        <a
+          target="_blank"
+          href="https://github.com/SiddDevZ"
+          className="ri-github-fill sm:text-[2.2rem] xss:text-[2rem] text-black dark:text-white transition-all duration-300 ease-in-out hover:scale-110 cursor-pointer"
+        ></a>
       </div>
     </div>
   );
