4506a4d4f5f1a0180f647e858377883da4eb983a
SiddDevZ <epic.siddarth@gmail.com>
2025-03-09 22:49:26 +0530

    Layout Updated

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,25 @@
-"use client";
 import "./globals.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import Head from 'next/head';
+
+const metadata = {
+  title: 'UnchainedGPT',
+  description: 'AI-powered platform by Team Unchained',
+  icons: {
+    icon: '/unchained.png',
+    shortcut: '/unchained.png',
+    apple: '/unchained.png',
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <link rel="icon" href={metadata.icons.icon} />
+      </head>
       <body>
         <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}>
           {children}
